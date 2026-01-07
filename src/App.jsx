9f95b4d039285f32e272517bb5ae45aff62e4737
fabrizio <serrano24112002@gmail.com>
2026-01-07 20:01:12 -0300

    se realizo la pagina de aboutus sujeto a mejoras

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import Home from './pages/home/home';
 import Aboutus from './pages/aboutus/aboutus';
 import Navb from './pages/navBar/navbar';
+import AboutUs from './pages/aboutus/aboutus';
+import QuienesSomos from './pages/aboutus/aboutus';
 
 function App() {
 
@@ -11,7 +13,7 @@ function App() {
   return (
    <BrowserRouter>
    <Routes>
-      <Route path='/aboutus' element={<Aboutus />} />
+      <Route path='/aboutus' element={<QuienesSomos />} />
       <Route path='/home' element={<Home />} />
       <Route path='/' element={<Navb />} />
    </Routes>
@@ -19,4 +21,22 @@ function App() {
   )
 }
 
-export default App;
+export default App;export const team = [
+  {
+    name: "Agus Valverdi",
+    img: "/assets/team/persona1.jpg",
+  },
+  {
+    name: "Gaston Suarez",
+    img: "../../img/foto.jpeg",
+  },
+  {
+    name: "Serrano Fabrizio",
+    img: "fabrizio.jpg",
+  },
+  {
+    name: "Ivan Robles",
+    img: "/assets/team/persona4.jpg",
+  },
+];
+
